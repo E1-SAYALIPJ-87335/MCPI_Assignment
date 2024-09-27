bc9753e45edcdcece1fbfd61400ddfcd81c46da6
E2_62876_manish <mdeore.md@gmail.com>
2024-09-27 17:51:19 +0530

    ASSIGNMENT

--- ASSIGNMENT4/Src/main.c
+++ ASSIGNMENT4/Src/main.c
@@ -20,77 +20,26 @@
 #include <stdio.h>
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
-
+#include "uart.h"
 #include "switch_intr.h"
 #include "led.h"
+#include "string.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-/*
-Orange LED will blink when switch is pressed -- Any Optimization Level
-As flag is declared volatile.
-*/
-
 int main(void) {
+	char str[32];
 	SystemInit();
-	SwitchInit();
-	LedInit(LED_ORANGE_PIN);
-	LedInit(LED_GREEN_PIN);
-	LedInit(LED_RED_PIN);
-	LedInit(LED_BLUE_PIN);
+	sprintf(str,"%d\r\n",Switchintcount);
+
 	while(1) {
-		// if switch is pressed blink orange led
-		//while(SwitchExtiFlag == 0)
-			;
-		/*// Optimization Level -- any
-			LDR r7, =SwitchExtiFlag 	// r7 = &SwitchExtiFlag;
-		loop:
-			LDR r0, [r7]				// r0 = *r7;
-			CMP r0, #0					// r0 == 0 (compare)
-			BEQ loop					// if 0, repeat loop
-		*/
-		LedBlink(LED_ORANGE_PIN, 1000);
-		DelayMs(1000);
-		//SwitchExtiFlag = 0;
-		while(1)
-			;
-		LedBlink(LED_GREEN_PIN, 1000);
-		DelayMs(1000);
-		//SwitchExtiFlag = 0;
-		while(1)
-			;
-		LedBlink(LED_RED_PIN, 1000);
-		DelayMs(1000);
-		//SwitchExtiFlag = 0;
-		while(1)
-			;
-		LedBlink(LED_BLUE_PIN, 1000);
-		DelayMs(1000);
-		SwitchExtiFlag = 0;
+		UartGets(str);
+
 	}
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
